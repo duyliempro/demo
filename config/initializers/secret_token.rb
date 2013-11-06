a445f0afc6df4eb32e494076832ec9c09449b742
TRANDUCLIEM <tranducliem@tranducliem-Inspiron-3537.(none)>
2013-11-06 16:43:26 +0700

    Firt Deme App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '424975028fe446ded020c6d7e079d73e210401c570561e2ffb34b75c4570cb4663717ebe6e17ccac0f507a344c426de6616a6b6b261d0c0a43b053ca6b573fd3'
