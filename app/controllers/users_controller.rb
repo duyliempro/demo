35ea0d642904774e6dae2bcd3310c68495729db6
tranducliem <duyliempro@gmail.com>
2013-11-07 15:26:44 +0700

    Make a basic User model (including secure passwords)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    #@user = User.all
+    render 'new'
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User created!"
+      redirect_to root_url
+    else
+      flash[:error] = "Failed to create User!"
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
